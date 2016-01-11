7bfdcd2d99197829c7c018d2817dfcc3e708774d
mmmfang <mae.fang@gmail.com>
2016-01-11 14:02:25 -0500

    notes are stored in localstorage

--- www/js/notestore.js
+++ www/js/notestore.js
@@ -3,7 +3,22 @@ angular.module('mynotes.notestore', [])
 	//creating a service for data that needs to be shared amongst more than one 
 //controller, like all this notes. the factory's job is to return a service
 .factory('NoteStore', function(){
-  var notes = [];
+  
+  //var notes = [];
+  //when initializing we want to read notes from local storage
+  //but it will be in json format so we switch it back
+  var notes = angular.fromJson(window.localStorage['notes'] || '[]');
+  //this or is incase nothing in localstorage, then first use this empty array
+  //as a string so it will be converted into an array object by fromJson function
+
+
+//we want to persist notes in local storage as json
+//and we need to call it each time we want to save - create or edit
+function persist() {
+	//window.localStorage['notes'] = notes
+	//convert to
+	window.localStorage['notes'] = angular.toJson(notes);
+}
 
   return {
     list: function(){
@@ -21,6 +36,7 @@ angular.module('mynotes.notestore', [])
 
     create: function(note){
         notes.push(note);
+        persist();
     },
 
     update: function(note) {
@@ -28,6 +44,7 @@ angular.module('mynotes.notestore', [])
         if (notes[i].id===note.id) {
           notes[i] = note;
           //in line above, we replace the note with our modified note
+          persist();
           return;
         }
       }
