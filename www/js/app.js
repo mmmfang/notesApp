b76e0d8c3e3100ccd86ae8f1c13099a1e36478c5
mmmfang <mae.fang@gmail.com>
2016-01-10 19:05:38 -0500

    edit is working using ng-model for two way binding

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,11 @@
 // Ionic Starter App
 
+(function(){
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('starter', ['ionic']);
 
-app.controller("ListCtrl", function($scope){
-
-  $scope.notes = [
-    {
-      title: 'First Note',
-      description: "This is my first note"
-    }, {
-      title: 'Second Note',
-      description: "This is my second note"
-    }
-    ];
-});
 
 //stateprovider service allows u to create new states
 app.config(function($stateProvider, $urlRouterProvider){
@@ -25,13 +14,55 @@ app.config(function($stateProvider, $urlRouterProvider){
     templateUrl: 'templates/list.html'
   });
   $stateProvider.state('edit', {
-    url:'/edit',
+    url:'/edit/:noteId',
     templateUrl: 'templates/edit.html'
   });
 
   $urlRouterProvider.otherwise('/list');
 });
 //the last line says if url doesn't match anything, go to '/list'
+
+
+ var notes = [
+    {
+      id: "1",
+      title: 'First Note',
+      description: "This is my first note"
+    }, {
+      id: "2",
+      title: 'Second Note',
+      description: "This is my second note"
+    }
+    ];
+
+
+app.controller("ListCtrl", function($scope){
+  $scope.notes = notes;
+//now just refers to the notes variable that is outside
+});
+
+
+
+//created this function outside of controllers to help us get the note per the noteID
+//that is needed in edit controller
+function getNote(noteId) {
+  for (var i=0; i<notes.length; i++) {
+    if (notes[i].id===noteId) {
+      return notes[i];
+    }
+  }
+}
+
+//****** $state - will let us access the parameter we get from the URL 
+//here it is the id of the note
+app.controller('EditCtrl', function($scope, $state){
+  //$scope.noteId = $state.params.noteId;
+  //this will set a variable on scope with our noteID
+  
+  //this is to have a note object on the scope
+  $scope.note = getNote($state.params.noteId);
+})
+
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -49,4 +80,12 @@ app.run(function($ionicPlatform) {
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+
+})(); 
+
+//wrapping everything in function. want the data from the $scope.notes (previously was just 
+  //located in listCtrl, to be available to other controllers. you can place it outside the 
+  //list Ctrl, but dont want it to be hanging out in the global scope, so wrap everything
+  //in an anonymous function
